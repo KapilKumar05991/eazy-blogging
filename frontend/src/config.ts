5f66e7de420a53e91859d663f96c0c520ef3f57a
Kapil Kumar <kapilkumar05991@gmail.com>
2025-06-22 08:58:20 +0530

    finalCommit

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = 'https://backend.kapilkumar05991.workers.dev'
-export const UPLOAD_PRESET = 'eazy-blogging-images'
-export const CLOUD_NAME = 'dxabgqgj3'
+export const BACKEND_URL = ''
+export const UPLOAD_PRESET = ''
+export const CLOUD_NAME = ''
